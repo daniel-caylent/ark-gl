95d156a64af85b10b2e48b02fdb1af07820ae6d1
Jorge Goldman <jorgegoldman@Jorges-MacBook-Pro.local>
2023-04-18 17:31:03 -0300

    ARK-4: adding indexes for uuid

---
+++ db/scripts/create_index.sql
@@ -0,0 +1,9 @@
+CREATE INDEX line_item_uuid_idx ON ARKGL.line_item(uuid);
+CREATE INDEX account_uuid_idx ON ARKGL.account(uuid);
+CREATE INDEX account_attribute_uuid_idx ON ARKGL.account_attribute(uuid);
+CREATE INDEX account_type_uuid_idx ON ARKGL.account_type(uuid);
+
+CREATE INDEX attachment_uuid_idx ON ARKGL.attachment(uuid);
+CREATE INDEX journal_entry_uuid_idx ON ARKGL.journal_entry(uuid);
+CREATE INDEX ledger_uuid_idx ON ARKGL.ledger(uuid);
+CREATE INDEX fund_entity_uuid_idx ON ARKGL.fund_entity(uuid);
