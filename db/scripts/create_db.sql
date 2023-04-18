a60dea4153162e74a58105e3f3e463c3b9ed6d89
Jorge Goldman <jorgegoldman@Jorges-MacBook-Pro.local>
2023-04-18 11:16:03 -0300

    ARK-4: First commit of db creation scripts

---
+++ db/scripts/create_db.sql
@@ -0,0 +1,122 @@
+drop table if exists ARKGL.attachment;
+drop table if exists ARKGL.journal_entry;
+drop table if exists ARKGL.ledger;
+drop table if exists ARKGL.fund_entity;
+
+drop table if exists ARKGL.line_item;
+drop table if exists ARKGL.account;
+drop table if exists ARKGL.account_attribute;
+drop table if exists ARKGL.account_type;
+
+CREATE TABLE IF NOT EXISTS ARKGL.fund_entity (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	`uuid` VARCHAR(30),
+	client_id VARCHAR(30),
+	fund_id VARCHAR(30),
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS ARKGL.ledger  (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	`uuid` VARCHAR(30),
+	fund_entity_id INT NOT NULL,
+	name VARCHAR(255),
+	description VARCHAR(255),
+	state VARCHAR(30),
+	is_hidden BOOL,
+	currency VARCHAR(255),
+	`decimal` INT,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY (fund_entity_id) REFERENCES fund_entity (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS ARKGL.journal_entry  (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	`uuid` VARCHAR(30),
+	ledger_id INT NOT NULL,
+	`date` TIMESTAMP,
+	reference VARCHAR(255),
+	memo VARCHAR(255),
+	adjusting_journal_entry BOOL,
+	state VARCHAR(30),
+	is_hidden BOOL,
+	post_date TIMESTAMP,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY (ledger_id) REFERENCES ledger (id)
+);
+
+CREATE TABLE IF NOT EXISTS ARKGL.account_type (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	`uuid` VARCHAR(30),
+	name VARCHAR(255),
+	description VARCHAR(255),
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	
+);
+
+
+CREATE TABLE IF NOT EXISTS ARKGL.account_attribute (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	`uuid` VARCHAR(30),
+	fund_entity_id INT NOT NULL,
+	account_type_id INT NOT NULL,
+	detail_type VARCHAR(255),
+	description VARCHAR(255),
+	state VARCHAR(30),
+	is_hidden BOOL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY (fund_entity_id) REFERENCES fund_entity(id),
+	FOREIGN KEY (account_type_id) REFERENCES account_type(id)
+);
+
+
+
+
+CREATE TABLE IF NOT EXISTS ARKGL.account (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	`uuid` VARCHAR(30),
+	fund_entity_id INT NOT NULL,
+	account_attribute_id INT,
+	parent_id INT DEFAULT 0,
+	name VARCHAR(255),
+	description VARCHAR(255),
+	state VARCHAR(30),
+	is_hidden bool,
+	is_taxable bool,
+	is_vendor_customer_partner_required bool,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY (fund_entity_id) REFERENCES fund_entity(id),
+	FOREIGN KEY (account_attribute_id) REFERENCES account_attribute(id),
+	FOREIGN KEY (parent_id) REFERENCES account(id) 
+);
+
+CREATE TABLE IF NOT EXISTS ARKGL.line_item (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	`uuid` VARCHAR(30),
+	account_id INT NOT NULL,
+	journal_entry_id INT NOT NULL,
+	line_number INT,
+	memo VARCHAR(255),
+	vendor_customer_partner_type VARCHAR(255),
+	vendor_customer_partner_id VARCHAR(255),
+	posting_type VARCHAR(30),
+	amount DOUBLE,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY (journal_entry_id) REFERENCES journal_entry(id),
+	FOREIGN KEY (account_id) REFERENCES account(id)
+);
+
+
+
+
+CREATE TABLE IF NOT EXISTS ARKGL.attachment (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	`uuid` VARCHAR(30),
+	journal_entry_id INT NOT NULL,
+	location VARCHAR(255),
+	memo VARCHAR(255),
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY (journal_entry_id) REFERENCES journal_entry(id)
+);
