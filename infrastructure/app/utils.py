c3cd706564c17749812b44e213f078b6844663b5
Daniel Slosky <daniel.slosky@caylent.com>
2023-04-21 18:36:06 +0000

    Squashed commit of the following
    commit 62ab928c 
    Author: Daniel Slosky <daniel.slosky@caylent.com> 
    Date: Fri Apr 21 2023 14:59:36 GMT-0300 (Brasilia Standard Time) 
    
        Fix app path for deployment

--- infrastructure/app/utils.py
+++ infrastructure/app/utils.py
@@ -1,7 +1,7 @@
 from pathlib import Path, PurePath
 
 THIS_DIRECTORY = Path(__file__).parent.absolute()
-APP_DIR = str(PurePath(THIS_DIRECTORY.parent.parent, 'app'))
+APP_DIR = str(PurePath(THIS_DIRECTORY.parent.parent, 'app', 'v1'))
 ACCOUNTS_DIR = str(PurePath(APP_DIR, 'accounts'))
 ACCOUNTS_ATTR_DIR = str(PurePath(APP_DIR, 'account_attributes'))
 LAYERS_DIR = str(PurePath(APP_DIR, 'layers'))
