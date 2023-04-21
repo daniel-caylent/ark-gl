763d28af95304bd9860f379431120c059c62dae4
Vinicius Defeo <vinicius.defeo@caylent.com>
2023-04-21 12:28:28 -0300

    minor fix

--- infrastructure/app/base_stack.py
+++ infrastructure/app/base_stack.py
@@ -5,7 +5,9 @@ from typing import Dict, Any, Mapping
 import os
 
 class BaseStack(Stack):
+
     def __init__(self, scope: Construct | None = None, id: str | None = None, *, analytics_reporting: bool | None = None, cross_region_references: bool | None = None, description: str | None = None, env: Environment | Dict[str, Any] | None = None, permissions_boundary: PermissionsBoundary | None = None, stack_name: str | None = None, synthesizer: IStackSynthesizer | None = None, tags: Mapping[str, str] | None = None, termination_protection: bool | None = None) -> None:
-        prefix = os.getenv('DEPLOY_ENVIRONEMT') + '-' if os.getenv('DEPLOY_ENVIRONEMT') is not None else ''
-        id = id if id is None else os.getenv('DEPLOY_ENVIRONEMT') + prefix + id
+        DEPLOYMENT_ENV = 'DEPLOYMENT_ENV'
+        prefix = os.getenv(DEPLOYMENT_ENV) + '-' if os.getenv(DEPLOYMENT_ENV) is not None else ''
+        id = id if id is None else prefix + id
         super().__init__(scope, id, analytics_reporting=analytics_reporting, cross_region_references=cross_region_references, description=description, env=env, permissions_boundary=permissions_boundary, stack_name=stack_name, synthesizer=synthesizer, tags=tags, termination_protection=termination_protection)
