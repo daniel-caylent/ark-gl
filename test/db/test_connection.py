9eaf7bdd4d0a16344efcdc29b38b352ce779a523
Jorge Goldman <jorge.goldman@caylent.com>
2023-04-27 15:18:46 +0000

    Squashed commit of the following
    commit 8d0559d8 
    Author: Jorge Goldman <jorgegoldman@ip-192-168-0-127.ec2.internal> 
    Date: Thu Apr 27 2023 11:39:23 GMT-0300 (Argentina Standard Time) 
    
        ARK-48: mocked db connection and cursor

--- test/db/test_connection.py
+++ test/db/test_connection.py
@@ -10,18 +10,19 @@ import app.v1.layers.database.python.database.connection as connection
 class TestConnection(unittest.TestCase):
 
     def setUp(self):
-        self.conn = connection.get_connection(db_name='ARKGL', region_name='us-east-2', secret_name='ark/db-password')
-        self.cursor = self.conn.cursor()
+        self.conn = mock.MagicMock()
+        self.cursor = mock.MagicMock()
+        self.cursor.fetchall.return_value = ['aRow']
 
     def tearDown(self):
         self.cursor.close()
         self.conn.close()
 
-    @unittest.skip("should replace the connection toa mocked object")
+    #@unittest.skip("should replace the connection toa mocked object")
     def test_get_connection(self):
         self.assertIsNotNone(self.conn)
 
-    @unittest.skip("should replace the connection toa mocked object")
+    #@unittest.skip("should replace the connection toa mocked object")
     def test_query(self):
         self.cursor.execute("SELECT * FROM account")
         result = self.cursor.fetchall()
