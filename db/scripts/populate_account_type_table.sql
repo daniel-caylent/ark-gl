63fbb4af212fed0268da8ceacfa7390dd7de10ab
Jorge Goldman <jorge.goldman@caylent.com>
2023-05-01 13:11:14 +0000

    Squashed commit of the following
    commit 5eab383a 
    Author: Jorge Goldman <jorgegoldman@Jorges-MacBook-Pro.local> 
    Date: Fri Apr 28 2023 15:21:28 GMT-0300 (Argentina Standard Time) 
    
        ARK-70: populate account type table

---
+++ db/scripts/populate_account_type_table.sql
@@ -0,0 +1,23 @@
+INSERT INTO ARKGL.account_type
+(uuid, name, description, created_at)
+VALUES(UUID(), 'Assets', 'Balance Sheet', CURRENT_TIMESTAMP);
+
+INSERT INTO ARKGL.account_type
+(uuid, name, description, created_at)
+VALUES(UUID(), 'Liabilities', 'Balance Sheet', CURRENT_TIMESTAMP);
+
+INSERT INTO ARKGL.account_type
+(uuid, name, description, created_at)
+VALUES(UUID(), 'Partners Capital', 'Balance Sheet', CURRENT_TIMESTAMP);
+
+INSERT INTO ARKGL.account_type
+(uuid, name, description, created_at)
+VALUES(UUID(), 'Income', 'Balance Sheet', CURRENT_TIMESTAMP);
+
+INSERT INTO ARKGL.account_type
+(uuid, name, description, created_at)
+VALUES(UUID(), 'Expense', 'Balance Sheet', CURRENT_TIMESTAMP);
+
+INSERT INTO ARKGL.account_type
+(uuid, name, description, created_at)
+VALUES(UUID(), 'Gain/Loss', 'Balance Sheet', CURRENT_TIMESTAMP);
