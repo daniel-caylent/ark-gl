c2febd0ca15e27a58136f4cb1ba0e1d8ab8d4d16
Franco Balduzzi <franco.balduzzi@caylent.com>
2023-05-16 18:58:11 +0000

    Squashed commit of the following
    commit 143d90f4 
    Author: Franco Balduzzi <francobalduzzi@Francos-MBP.fibertel.com.ar> 
    Date: Tue May 16 2023 11:57:35 GMT-0300 (Argentina Standard Time) 
    
        Adding first draft of lambda code for ledger's reconciliation

--- app/v1/reconciliation/ledgers/ledger.py
+++ app/v1/reconciliation/ledgers/ledger.py
@@ -1,4 +1,43 @@
+# import json
+from ark_qldb import qldb
+# from random import random
+from arkdb import ledgers
 
 def handler(event, context) -> tuple[int, dict]:
+    
+    driver = qldb.Driver("ARKGL", region_name='us-east-1')
+    buffered_cursor = driver.read_documents("ledger")
+    processed_list = []
+    processed_succesfully = []
+    processed_failure = []
+    
+    for current_row in buffered_cursor:
+        processed_success = True
+        current_uuid = current_row['uuid']
+        
+        aurora_record = ledgers.select_by_id(current_uuid)
+        if aurora_record is None:
+            print("Error") #record exists in QLDB and not in Aurora. Someone deleted it
+            processed_success = False
+        else:
+            for current_key in current_row.keys():
+                if aurora_record.get(current_key) is None:
+                    print("Key "+ current_key + " does not exist in Aurora " ) #key does not exist in Aurora 
+                    processed_success = False
+                else:    
+                    if aurora_record[current_key] != current_row[current_key]:
+                        print("Error on value for key "+ current_key ) #record value mistmatch. Someone tampered with the DB
+                        processed_success = False
+            
+        processed_list.append(current_row)
+        if processed_success:
+            processed_succesfully.append(current_row)
+        else:
+            processed_failure.append(current_row)
+            
+    account_count = ledgers.select_count_commited_accounts()
+    if account_count['count(*)'] != len(processed_list):
+        print("Error on amount of records on Aurora "+str(account_count['count(*)'] )+ " vs QLDB "+ str(len(processed_list)) ) # distintc amount of accounts in the QLDB than the DB 
 
+    
     return 200, {}
