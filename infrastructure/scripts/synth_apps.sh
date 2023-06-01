556dad6d6342af2b8a1bc7e2b259bea2492160b2
Vinicius Defeo <vinicius.defeo@caylent.com>
2023-06-01 20:36:09 -0300

    testing

--- infrastructure/scripts/synth_apps.sh
+++ infrastructure/scripts/synth_apps.sh
@@ -2,6 +2,8 @@
 set -e
 export AWS_ACCOUNT=319244063014
 export AWS_REGION=us-east-1
+export AWS_ACCESS_KEY_ID=dummy_access_key
+export AWS_SECRET_ACCESS_KEY=dummy_secret_key
 cdk synth --app "python3 ../app.py"
 cdk synth --app "python3 ../dr_app.py"
 cdk synth --app "python3 ../reconciliation_app.py"
