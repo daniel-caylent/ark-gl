152a13ea9d5b651ea038ed1d5103d16596f2ece7
Vinicius Defeo <vinicius.defeo@caylent.com>
2023-06-01 20:26:55 -0300

    fix synth

--- infrastructure/scripts/synth_apps.sh
+++ infrastructure/scripts/synth_apps.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 set -e
+export AWS_ACCOUNT=319244063014
+export AWS_REGION=us-east-1
 cdk synth --app "python3 ../app.py"
 cdk synth --app "python3 ../dr_app.py"
 cdk synth --app "python3 ../reconciliation_app.py"
