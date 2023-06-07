62eaba443883e19c04e8cf179e079955cbf6ff0b
Jorge Goldman <jorge.goldman@caylent.com>
2023-06-07 20:03:19 +0000

    Squashed commit of the following
    commit 9e4f78ec 
    Author: Jorge Goldman <jorgegoldman@ip-192-168-0-127.ec2.internal> 
    Date: Wed Jun 07 2023 17:01:25 GMT-0300 (Argentina Standard Time) 
    
        ARK-167: fix date

--- db/scripts/create_views.sql
+++ db/scripts/create_views.sql
@@ -11,7 +11,7 @@ acc_att.account_type,
 le.uuid le_uuid,
 le.name le_name,
 le.currency,
-je.date,
+je.post_date je_post_dat,
 je.state je_state,
 sum(case when li.posting_type = 'CREDIT' then li.amount else 0 end) as "CREDIT",
 sum(case when li.posting_type = 'DEBIT' then li.amount*(-1) else 0 end) as "DEBIT",
@@ -59,15 +59,37 @@ where acc_att.account_type not in ('Assets', 'Liabilities','Partners Capital')
 -- and account.post_date between ? and ?
 group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15;
 
+CREATE OR REPLACE VIEW 1099_VW AS
+SELECT
+fe.uuid fe_uuid,
+le.uuid le_uuid,
+li.entity_id li_entity_id,
+je.post_date je_post_date,
+je.state je_state,
+sum(case when li.posting_type = 'CREDIT' then li.amount else 0 end)+
+sum(case when li.posting_type = 'DEBIT' then li.amount*(-1) else 0 end) as "TOTAL"
+from 
+line_item li 
+inner join journal_entry je on li.journal_entry_id  = je.id
+ inner join ledger le on le.id = je.ledger_id  
+ inner join fund_entity fe on le.fund_entity_id = fe.id
+ inner join account acc on acc.id = li.account_id 
+ inner join account_attribute acc_att on acc_att.id = acc.account_attribute_id
+-- and fe.client_id = ?
+-- and le.uuid = ?
+-- and fe.fund_id = ?
+-- and account.post_date between ? and ?
+group by 1,2,3,4,5;
+
 CREATE OR REPLACE VIEW TRIAL_BALANCE_VW AS
 SELECT
 fe.uuid fe_uuid,
 acc.uuid acc_uuid,
-acc.account_no,
+je.journal_entry_num,
 acc.name name,
 concat(acc.name, ' ', acc.account_no) acc_app_name,
 le.uuid le_uuid,
-je.date je_date,
+je.post_date je_post_date,
 je.state je_state,
 sum(case when li.posting_type = 'CREDIT' then li.amount else 0 end) as "CREDIT",
 sum(case when li.posting_type = 'DEBIT' then li.amount else 0 end) as "DEBIT"
@@ -88,16 +110,15 @@ CREATE OR REPLACE VIEW DETAILED_TRIAL_BALANCE_VW AS
 SELECT
 fe.uuid fe_uuid,
 acc.uuid acc_uuid,
-acc.account_no,
+je.journal_entry_num,
 acc.name name,
 concat(acc.name, ' ', acc.account_no) acc_app_name,
 li.line_number ,
 li.memo, 
 le.uuid le_uuid,
-je.date je_date,
+je.post_date je_post_date,
 je.state je_state,
-sum(case when li.posting_type = 'CREDIT' then li.amount else 0 end) +
-sum(case when li.posting_type = 'DEBIT' then li.amount*(-1) else 0 end) as "AMOUNT"
+case when li.posting_type = 'CREDIT' then li.amount else li.amount*(-1)  end as "Amount"
 from 
 line_item li 
 inner join journal_entry je on li.journal_entry_id  = je.id
@@ -111,27 +132,6 @@ inner join journal_entry je on li.journal_entry_id  = je.id
 -- and account.post_date between ? and ?
 group by 1,2,3,4,5,6,7,8,9,10;
 
-CREATE OR REPLACE VIEW 1099_VW AS
-SELECT
-fe.uuid fe_uuid,
-le.uuid le_uuid,
-li.entity_id li_entity_id,
-je.date je_date,
-je.state je_state,
-sum(case when li.posting_type = 'CREDIT' then li.amount else 0 end)+
-sum(case when li.posting_type = 'DEBIT' then li.amount*(-1) else 0 end) as "TOTAL"
-from 
-line_item li 
-inner join journal_entry je on li.journal_entry_id  = je.id
- inner join ledger le on le.id = je.ledger_id  
- inner join fund_entity fe on le.fund_entity_id = fe.id
- inner join account acc on acc.id = li.account_id 
- inner join account_attribute acc_att on acc_att.id = acc.account_attribute_id
--- and fe.client_id = ?
--- and le.uuid = ?
--- and fe.fund_id = ?
--- and account.post_date between ? and ?
-group by 1,2,3,4,5;
 
 CREATE OR REPLACE VIEW DETAILED_1099_VW AS
 SELECT
@@ -140,9 +140,27 @@ le.uuid le_uuid,
 je.journal_entry_num,
 li.memo,
 li.entity_id li_entity_id,
-je.date je_date,
+je.post_date je_post_date,
 je.state je_state,
-case when li.posting_type = 'CREDIT' then li.amount else li.amount*(-1)  end as "TOTAL"
+case when li.posting_type = 'CREDIT' then li.amount else li.amount*(-1)  end as "Amount"
+from 
+line_item li 
+inner join journal_entry je on li.journal_entry_id  = je.id
+ inner join ledger le on le.id = je.ledger_id  
+ inner join fund_entity fe on le.fund_entity_id = fe.id
+ inner join account acc on acc.id = li.account_id 
+ inner join account_attribute acc_att on acc_att.id = acc.account_attribute_id;
+-- and fe.client_id = ?
+-- and le.uuid = ?
+-- and fe.fund_id = ?
+-- and account.post_date between ? and ?
+-- group by 1,2,3,4,5,6,7;
+ 
+CREATE OR REPLACE VIEW BALANCE_FOR_DETAILED_1099_VW AS
+SELECT
+le.uuid le_uuid,
+je.post_date je_post_date,
+sum(case when li.posting_type = 'CREDIT' then li.amount else li.amount*(-1)  end ) as "TOTAL"
 from 
 line_item li 
 inner join journal_entry je on li.journal_entry_id  = je.id
