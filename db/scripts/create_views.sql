68a62123433fb1d0b8e0e779a1696fef0ee65150
Jorge Goldman <jorge.goldman@caylent.com>
2023-06-07 13:14:10 +0000

    Squashed commit of the following
    commit f6388312 
    Author: Jorge Goldman <jorgegoldman@ip-192-168-0-127.ec2.internal> 
    Date: Wed Jun 07 2023 10:12:49 GMT-0300 (Argentina Standard Time) 
    
        ARK-164: Added detailed trial balance view

--- db/scripts/create_views.sql
+++ db/scripts/create_views.sql
@@ -84,6 +84,33 @@ inner join journal_entry je on li.journal_entry_id  = je.id
 -- and account.post_date between ? and ?
 group by 1,2,3,4,5,6,7,8;
 
+CREATE OR REPLACE VIEW DETAILED_TRIAL_BALANCE_VW AS
+SELECT
+fe.uuid fe_uuid,
+acc.uuid acc_uuid,
+acc.account_no,
+acc.name name,
+concat(acc.name, ' ', acc.account_no) acc_app_name,
+li.line_number ,
+li.memo, 
+le.uuid le_uuid,
+je.date je_date,
+je.state je_state,
+sum(case when li.posting_type = 'CREDIT' then li.amount else 0 end) +
+sum(case when li.posting_type = 'DEBIT' then li.amount*(-1) else 0 end) as "AMOUNT"
+from 
+line_item li 
+inner join journal_entry je on li.journal_entry_id  = je.id
+ inner join ledger le on le.id = je.ledger_id  
+ inner join fund_entity fe on le.fund_entity_id = fe.id
+ inner join account acc on acc.id = li.account_id 
+ inner join account_attribute acc_att on acc_att.id = acc.account_attribute_id
+-- and fe.client_id = ?
+-- and le.uuid = ?
+-- and fe.fund_id = ?
+-- and account.post_date between ? and ?
+group by 1,2,3,4,5,6,7,8,9,10;
+
 CREATE OR REPLACE VIEW 1099_VW AS
 SELECT
 fe.uuid fe_uuid,
