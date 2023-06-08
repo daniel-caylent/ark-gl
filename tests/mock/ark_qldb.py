e15cf1710f381327bf9b7b4c870aa4b815670867
Daniel Slosky <daniel.slosky@caylent.com>
2023-06-08 09:54:24 -0600

    Fix tests

---
+++ tests/mock/ark_qldb.py
@@ -0,0 +1,2 @@
+def post(table, item):
+    return
--- tests/mock/arkdb/journal_entries.py
+++ tests/mock/arkdb/journal_entries.py
@@ -1,4 +1,5 @@
 def select_by_ledger_id(id):
+    """Mock select by ledger id for journal entries"""
     return [
         {
             "ledgerId": "ab353519-eb7c-11ed-9a6e-0a3efd619f29",
@@ -32,7 +33,8 @@ def select_by_ledger_id(id):
         },
     ]
 
-def get_line_items(id):
+def get_line_items(id, translate=True):
+    """Mock select get_line_items for journal entries"""
     return [
         {
             "accountNo": "5555602528",
@@ -52,7 +54,8 @@ def get_line_items(id):
         }
       ]
 
-def get_attachments(id):
+def get_attachments(id, translate=True):
+    """Mock select get_attachments for journal entries"""
     return [
         {
             "documentId": "This is the id",
@@ -60,7 +63,8 @@ def get_attachments(id):
         }
     ]
 
-def select_by_id(id):
+def select_by_id(id, translate=True):
+    """Mock select select_by_id for journal entries"""
     if id == "a92bde1e-7825-429d-aaae-909f2d7a8df2":
         return {
             "ledgerId": "ab353519-eb7c-11ed-9a6e-0a3efd619f29",
@@ -95,15 +99,19 @@ def select_by_id(id):
         }
 
 def create_new(*args):
+    """Mock select create_new for journal entries"""
     return 'a unique id'
 
 def update_by_id(id, input):
+    """Mock update for journal entries"""
     return
 
 def delete_by_id(id):
+    """Mock delete for journal entries"""
     return
 
 def select_lines_by_journals(id):
+    """Mock select line by journals for journal entries"""
     return [
         {
             "id" : 5,
@@ -121,6 +129,7 @@ def select_lines_by_journals(id):
     ]
 
 def select_attachments_by_journals(id):
+    """Mock select attachments for journal entries"""
     return [
         {
 		    "id" : 2,
