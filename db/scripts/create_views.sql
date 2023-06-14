37364e17889a423bf43b08173efd91e999509f89
Jorge Goldman <jorge.goldman@caylent.com>
2023-06-14 15:03:24 +0000

    Squashed commit of the following
    commit 83255bbe 
    Author: Jorge Goldman <jorgegoldman@ip-192-168-0-127.ec2.internal> 
    Date: Wed Jun 14 2023 11:31:18 GMT-0300 (Argentina Standard Time) 
    
        ARK-163: first version of real income statement report

--- db/scripts/create_views.sql
+++ db/scripts/create_views.sql
@@ -31,23 +31,19 @@ where acc_att.detail_type = 'Balance Sheet'
 group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15;
 
 CREATE OR REPLACE VIEW INCOME_STATEMENT_VW AS 
-SELECT fe.uuid fund_uuid,
-fe.client_id,
-fe.fund_id,
-acc.id as acc_id,
+SELECT 
+fe.uuid fe_uuid,
+fe.client_id, 
 acc.uuid acc_uuid,
-acc.account_no,
-acc.parent_id, 
-acc.fs_mapping_id,
-acc_att.account_type,
 le.uuid le_uuid,
-le.name le_name,
+acc.account_no,
+acc_att.account_type,
 le.currency,
 li.posting_type,
-je.date je_date,
 je.state je_state,
 CONCAT(QUARTER(je.date), " ", YEAR(je.date)) as "QUARTER",
-sum(li.amount)
+sum(case when li.posting_type = 'CREDIT' then li.amount else 0 end) as "CREDIT",
+sum(case when li.posting_type = 'DEBIT' then li.amount*(-1) else 0 end) as "DEBIT"
 FROM 
 line_item li 
 inner join journal_entry je on li.journal_entry_id  = je.id
@@ -55,12 +51,12 @@ inner join journal_entry je on li.journal_entry_id  = je.id
  inner join fund_entity fe on le.fund_entity_id = fe.id
  inner join account acc on acc.id = li.account_id 
  inner join account_attribute acc_att on acc_att.id = acc.account_attribute_id
-where acc_att.account_type not in ('Assets', 'Liabilities','Partners Capital')
+where acc_att.detail_type  like ('Income Statement')
 -- and fe.client_id = ?
 -- and le.uuid = ?
 -- and fe.fund_id = ?
 -- and account.post_date between ? and ?
-group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16;
+group by 1,2,3,4,5,6,7,8,9,10;
 
 CREATE OR REPLACE VIEW 1099_VW AS
 SELECT
