42f9f51024cb2e81c8f59bdf29257ea1f59fe3a6
Jorge Goldman <jorge.goldman@caylent.com>
2023-06-15 13:58:48 +0000

    Squashed commit of the following
    commit 51c3782d 
    Author: Jorge Goldman <jorgegoldman@ip-192-168-0-127.ec2.internal> 
    Date: Thu Jun 15 2023 10:55:50 GMT-0300 (Argentina Standard Time) 
    
        ARK-226: added field to detailed reports

--- db/scripts/create_views.sql
+++ db/scripts/create_views.sql
@@ -119,6 +119,7 @@ li.line_number ,
 li.memo, 
 le.uuid le_uuid,
 je.post_date je_post_date,
+je.adjusting_journal_entry,
 je.state je_state,
 je.date je_date,
 case when li.posting_type = 'CREDIT' then li.amount else li.amount*(-1)  end as "Amount"
@@ -133,7 +134,7 @@ inner join journal_entry je on li.journal_entry_id  = je.id
 -- and le.uuid = ?
 -- and fe.fund_id = ?
 -- and account.post_date between ? and ?
-group by 1,2,3,4,5,6,7,8,9,10,11,12;
+group by 1,2,3,4,5,6,7,8,9,10,11,12,13;
 
 
 CREATE OR REPLACE VIEW DETAILED_1099_VW AS
@@ -144,6 +145,7 @@ je.journal_entry_num,
 li.memo,
 li.entity_id li_entity_id,
 je.post_date je_post_date,
+je.adjusting_journal_entry,
 je.state je_state,
 je.date je_date,
 case when li.posting_type = 'CREDIT' then li.amount else li.amount*(-1)  end as "Amount"
@@ -154,11 +156,6 @@ inner join journal_entry je on li.journal_entry_id  = je.id
  inner join fund_entity fe on le.fund_entity_id = fe.id
  inner join account acc on acc.id = li.account_id 
  inner join account_attribute acc_att on acc_att.id = acc.account_attribute_id;
--- and fe.client_id = ?
--- and le.uuid = ?
--- and fe.fund_id = ?
--- and account.post_date between ? and ?
--- group by 1,2,3,4,5,6,7;
  
 CREATE OR REPLACE VIEW BALANCE_FOR_DETAILED_1099_VW AS
 SELECT
@@ -192,6 +189,7 @@ le.uuid le_uuid,
 le.name le_name,
 le.currency,
 je.journal_entry_num,
+je.adjusting_journal_entry ,
 je.post_date je_post_date,
 je.state je_state,
 je.date je_date,
